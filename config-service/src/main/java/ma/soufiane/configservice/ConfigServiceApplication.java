bf10e7dc4886a05e1d396903371cc08ed728616e
SOUFIANE ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-10-20 09:45:52 +0100

    create config-service

---
+++ config-service/src/main/java/ma/soufiane/configservice/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.soufiane.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
